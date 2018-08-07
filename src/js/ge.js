ecfc240c35c03cb34f7152abc1f992088d32b42c
Alexis Horde <alexis.horde@ign.fr>
2018-08-07 10:33:02 +0200

    Reunion de jquery-geometry-editor et ol-geometry-editor

--- src/js/ge.js
+++ src/js/ge.js
@@ -4,6 +4,19 @@ require("../css/draw-control.css");
 global.ge = {
     defaultParams: require('./ge/defaultParams'),
     GeometryEditor: require('./ge/GeometryEditor')
-} ;
+};
+
+
+if (jQuery) {
+    /**
+     * Expose jQuery plugin
+     */
+    jQuery.fn.geometryEditor = function (options) {
+        return this.each(function () {
+            var editor = new global.ge.GeometryEditor(this, options);
+            $(this).data('editor', editor);
+        });
+    };
+}
 
 module.exports = global.ge;
--- webpack.config.js
+++ webpack.config.js
@@ -38,39 +38,5 @@ module.exports = [
             new ExtractTextWebpackPlugin("ol-geometry-editor.min.css"),
             new CopyWebpackPlugin([{from: 'src/images', to: 'images'}])
         ]
-    },
-    {
-        entry: "./src/js/ge-jquery.js",
-        output: {
-            path: path.resolve(__dirname, "./dist"),
-            filename: "./jquery-geometry-editor.min.js"
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.js$/,
-                    exclude: /node_modules/,
-                    loader: "babel-loader"
-                },
-                {
-                    test: /\.css$/,
-                    exclude: /node_modules/,
-                    use: ExtractTextWebpackPlugin.extract({
-                        fallback: 'style-loader',
-                        use: {loader: 'css-loader', options: {minimize: true}}
-                    })
-                },
-                {
-                    test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-                    loader: 'url-loader',
-                    options: {
-                        limit: 10000
-                    }
-                }
-            ]
-        },
-        plugins: [
-            new ExtractTextWebpackPlugin("ol-geometry-editor.min.css"),
-        ]
     }
 ];
