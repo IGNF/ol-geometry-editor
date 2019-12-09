287f41e1ea32b7c13c51da096dac8cf48a7a06b9
ahorde <alexis.horde@ign.fr>
2019-12-09 15:02:24 +0100

    Correction validation GeometryCollection isValidGeometry.js

--- src/js/ge/util/isValidGeometry.js
+++ src/js/ge/util/isValidGeometry.js
@@ -3,38 +3,58 @@
  * Indicates if the given geometry is valid
  * @param {String} geometryType tested geometry type
  */
-var isValidGeometry = function(geometry) {
+var isValidGeometry = function (geometry) {
 
-    // attribute 'type' is here ?
-    if(!geometry.type){
-        return false;
-    }
 
-    // value 'type' is good ?
-    switch (geometry.type){
-        case "Point":
-        case "LineString":
-        case "Polygon":
-        case "MultiPoint":
-        case "MultiLineString":
-        case "MultiPolygon":
-        case "GeometryCollection":
-            break;
-        default:
+    var isGeometryOK = function (geometry) {
+        // attribute 'type' is here ?
+        if (!geometry.type) {
             return false;
+        }
+
+        // value 'type' is good ?
+        switch (geometry.type) {
+            case "Point":
+            case "LineString":
+            case "Polygon":
+            case "MultiPoint":
+            case "MultiLineString":
+            case "MultiPolygon":
+            case "GeometryCollection":
+                break;
+            default:
+                return false;
+        }
+
+        // attribute 'coordinates' is here ?
+        if (!geometry.coordinates) {
+            return false;
+        }
+
+        // value 'coordinates' is coherent ?
+        if (new ol.format.GeoJSON().readGeometry(JSON.stringify(geometry)).getCoordinates().length == 0) {
+            return false;
+        }
+
+        return true;
     }
 
-    // attribute 'coordinates' is here ?
-    if(!geometry.coordinates){
-        return false;
-    }
 
-    // value 'coordinates' is coherent ?
-    if(new ol.format.GeoJSON().readGeometry(JSON.stringify(geometry)).getCoordinates().length == 0){
-        return false;
+
+    if (geometry.geometries) {
+        for (var i in geometry.geometries) {
+           if(!isGeometryOK(geometry.geometries[i])){
+            return false;
+           }
+        }
+    }else{
+        if(!isGeometryOK(geometry)){
+            return false;
+        }
     }
 
     return true;
+
 };
 
-module.exports = isValidGeometry ;
+module.exports = isValidGeometry;
